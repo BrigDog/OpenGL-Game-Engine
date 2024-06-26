5b900f67bb3c7f97dbfc017e9011d697490b71bc
Joshua Briggs <joshuabriggs0001@gmail.com>
2024-06-26 18:34:29 +0100

    Created 2 temporary shaders.
    1 vertex shader that can edit the UV positions.
    1 frag shader that can edit the colour of the triangle.

--- Source/Window.cpp
+++ Source/Window.cpp
@@ -1,6 +1,52 @@
 #include "../Header/Window.h"
 #include <cstddef>
 #include <cassert>
+#include <string>
+#include <iostream>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " <<
+            (type == GL_VERTEX_SHADER ? "vertex" : "pixel")
+            << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& pixelShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int ps = CompileShader(GL_FRAGMENT_SHADER, pixelShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, ps);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(ps);
+
+    return program;
+}
 
 int Window::Init()
 {
@@ -32,6 +78,26 @@ int Window::Init()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position.xy\n"
+        "}\n";
+    std::string pixelShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0,0.0,0.0,1.0);\n"
+        "}\n";
+    unsigned int shader = CreateShader(vertexShader, pixelShader);
+    glUseProgram(shader);
     return 0;
 }
 
